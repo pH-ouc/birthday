14ee1731345865d2acf1b8395f0c3187d874dbe1
pH-ouc <56378637+pH-ouc@users.noreply.github.com>
2021-02-22 20:33:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "旗子",
+        bannar_coming: "氛围",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
